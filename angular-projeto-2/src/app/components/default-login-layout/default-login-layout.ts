5ac48d358566b76df011d486c021d37f9f1ed3bb
anascassa <anacaroline25190@gmail.com>
2026-02-23 18:19:14 -0300

    exercise angular styles

---
+++ angular-projeto-2/src/app/components/default-login-layout/default-login-layout.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [],
+  templateUrl: './default-login-layout.html',
+  styleUrl: './default-login-layout.css',
+})
+export class DefaultLoginLayout {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondBtnText: string = "";
+}
